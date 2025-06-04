28d41166228f7642dddf44b6f0bbef66a8160d5c
iamkanguk97 <iamkanguk97@kakao.com>
2025-06-04 17:16:23 +0900

    feat(#2): User and UserAuth schema modeling

--- src/domains/User.js
+++ src/domains/User.js
@@ -1,3 +1,31 @@
-class User {}
+class User {
+    /**
+     * id: string | 고유ID (UUID) (PK)
+     * displayId: string | 표시용 ID (예: D0000001) (UNIQUE)
+     * emailAddress: string | 이메일 주소
+     * emailDomain: string | 이메일 도메인
+     * password: string | 비밀번호
+     * nickName: string | 닉네임 (UNIQUE)
+     * profileImageUrl: string | 프로필 이미지 URL (NULLABLE)
+     * createdAt: Date | 생성일시
+     * updatedAt: Date | 수정일시
+     * deletedAt: Date | 삭제일시
+     *
+     * @param {Object} data
+     */
+    constructor(data) {
+        // this.id = data.id;
+        // this.displayId = data.displayId;
+        // this.emailAddress = data.emailAddress;
+        // this.emailDomain = data.emailDomain;
+        // this.password = data.password;
+        // this.nickName = data.nickName;
+        // this.createdAt = data.createdAt;
+        // this.updatedAt = data.updatedAt;
+        // this.deletedAt = data.deletedAt;
+
+        Object.assign(this, data);
+    }
+}
 
 export default User;
---
+++ src/domains/UserAuth.js
@@ -0,0 +1,32 @@
+class UserAuth {
+    /**
+     * id: string | 고유ID (UUID) (PK)
+     * userId: string | 사용자ID (FK)
+     * sessionId: string | 세션ID
+     * status: string | 상태 (ACTIVE, INACTIVE, EXPIRED, REVOKED)
+     * ipAddress: string | IP주소
+     * userAgent: string | 사용자 에이전트
+     * createdAt: Date | 생성일시
+     * expiresAt: Date | 만료일시
+     * lastAccessedAt: Date | 마지막 접속일시
+     * revokedAt: Date | 취소일시
+     *
+     * @param {Object} data
+     */
+    constructor(data) {
+        // this.id = data.id;
+        // this.userId = data.userId;
+        // this.sessionId = data.sessionId;
+        // this.status = data.status;
+        // this.ipAddress = data.ipAddress;
+        // this.userAgent = data.userAgent;
+        // this.createdAt = data.createdAt;
+        // this.expiresAt = data.expiresAt;
+        // this.lastAccessedAt = data.lastAccessedAt;
+        // this.revokedAt = data.revokedAt;
+
+        Object.assign(this, data);
+    }
+}
+
+export default UserAuth;
