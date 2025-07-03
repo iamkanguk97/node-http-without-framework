73c312bb7cb84f973411565ffeaefff4fd7c9e33
iamkanguk97 <iamkanguk97@kakao.com>
2025-07-03 13:16:07 +0900

    remove: unnecessary test file

--- src/exceptions/ErrorMessage.js
+++ src/exceptions/ErrorMessage.js
@@ -7,6 +7,9 @@ export const ERROR_MESSAGE = Object.freeze({
     ROUTE_NOT_FOUND: { responseCode: 9005, message: '요청한 경로를 찾을 수 없습니다.' },
     METHOD_NOT_ALLOWED: { responseCode: 9006, message: '요청한 메서드를 허용하지 않습니다.' },
     CONFLICT: { responseCode: 9007, message: '요청한 리소스가 이미 존재합니다.' },
-    // INTERNAL_SERVER_ERROR: { responseCode: 9008, message: '서버 내부 오류가 발생했습니다.' }
-    UNCATCHED_ERROR: { responseCode: 9010, message: '알 수 없는 에러가 발생했습니다.' }
+    UNCATCHED_ERROR: { responseCode: 9010, message: '알 수 없는 에러가 발생했습니다.' },
+
+    /** 서버 내부에러 종류 */
+    INTERNAL_SERVER_ERROR: { responseCode: 8000, message: '서버 내부 오류가 발생했습니다.' },
+    INVALID_PREFIX: { responseCode: 8001, message: '유효하지 않은 Router Path Prefix 입니다.' }
 });
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,7 @@
 import Router from '../common/router/index.js';
 import { userController } from '../controllers/user.controller.js';
 
-const userRouter = new Router({ prefix: '/users' });
+const userRouter = new Router({ prefix: '/' });
 
 /**
  * Create a new user.
